2112dffa11bce171f8b1219cc8d745301dffbc3f
June <yoo.junehyuk@gmail.com>
2025-05-23 14:38:13 +0800

    Fixed some styling issues.

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -1,12 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar.tsx";
+import "../styles/app.css"
 
 const Root = () => {
     return (
-        <>
+        <div className="app">
             <Navbar />
             <Outlet />
-        </>
+        </div>
     );
 };
 
